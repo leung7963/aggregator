00bc421f4eef11b8feda635a97ca72ced00b10bf
leung7963 <104255180+leung7963@users.noreply.github.com>
2025-02-01 00:03:49 +0800

    更新 collect.py

--- subscribe/collect.py
+++ subscribe/collect.py
@@ -572,7 +572,7 @@ if __name__ == "__main__":
         "--url",
         type=str,
         required=False,
-        default="https://www.google.com/generate_204",
+        default="https://check.leung0108.us.kg",
         help="test url",
     )
 
